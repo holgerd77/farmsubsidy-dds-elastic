c9212d6e35d72a55f73a081c0f2826ee38fcbe5a
holgerd77 <Holger.Drewes@googlemail.com>
2016-03-04 13:31:06 +0100

    Added some (temporary) helper scripts

---
+++ recreate_index.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ./recreate_index.sh FILENAME(JSON Lines formatted/Elastic indexing format)" >&2
+  exit 1
+fi
+
+curl -XDELETE localhost:9200/_template/template_1
+curl -XPUT localhost:9200/_template/template_1 -d '@conf/template.json'
+curl -XDELETE 'localhost:9200/farmsubsidy-dds?pretty'
+curl -XPUT 'localhost:9200/farmsubsidy-dds/payment/_bulk?pretty' --data-binary "@$1"
+printf "\n"
+sleep 1
+curl 'localhost:9200/_cat/indices?v'
